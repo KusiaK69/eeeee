3d7219fef3ab01806d966cfc5edf40de0fee56ae
KusiaK69 <77748496+KusiaK69@users.noreply.github.com>
2021-03-25 10:35:51 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -82,7 +82,7 @@ const getRoomHash = (): string => {
 }
 
 const msg = `%c 
-    Script created by gbaranski#5119! 
+    Script created by KusiaK 
     https://github.com/gbaranski/quizizz-cheat
       `;
 
